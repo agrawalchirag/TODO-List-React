ab6fb460d7c9aad901ea62f2f4f0de4c6eb0de52
Chirag Agrawal <cagrawal@techracers.io>
2019-02-14 22:44:54 +0530

    Redirect Component has been added to the login file

--- my-app/src/containers/Login.js
+++ my-app/src/containers/Login.js
@@ -1,40 +1,46 @@
 import React from "react";
-import './Login.css'
+import './style.css';
+import { Redirect } from 'react-router-dom';
+
 class Login extends React.Component {
   constructor(props) {
     super(props);
-    this.state =  {value: ""};
+    this.state = {
+      value: '',
+      isSubmit: false
+    };
+
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  validateForm() {
-    return this.state.email.length > 0 && this.state.password.length > 0;
-  }
-
   handleChange(event) {
-    this.setState({value: event.target.value});
+    this.setState({ value: event.target.value });
   }
 
-  handleSubmit = event => {
-    event.preventDefault();
+  handleSubmit = () => {
+    this.setState(() => ({
+      isSubmit: true
+    }))
   }
 
   render() {
+    if (this.state.isSubmit === true) {
+      return <Redirect to='/todolist' />
+    }
     return (
-      <div>
-        <h1>Login Page</h1>
-        <form onSubmit={this.handleSubmit} align = "center">
-        <fieldset>
-          <legend>LOGIN</legend><br/>
-            Email:<br/>
-          <input type="text"  value={this.state.email} onChange={this.handleChange} required></input><br/>
-          <br/>
-            Password:<br/>
-          <input type="text"  value={this.state.password} onChange={this.handleChange} required></input><br/><br/>  
-          <input type="submit" ></input>
+      <div className="login-division">
+        <h1 className="heading">Login Page</h1>
+        <form onSubmit={this.handleSubmit} align="center">
+          <fieldset>
+            <legend>LOGIN</legend><br />
+            Email:<br />
+            <input className="input-field" type="text" value={this.state.email} onChange={this.handleChange} required></input><br />
+            <br />
+            Password:<br />
+            <input className="input-field" type="text" value={this.state.password} onChange={this.handleChange} required></input><br /><br />
+            <input type="submit" className="button"></input>
           </fieldset>
-          
         </form>
       </div>
     )
---
+++ my-app/src/containers/practice.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Practice extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>List of Tasks</h1>
+      </div>
+    )
+  }
+}
+
+export default Practice;
